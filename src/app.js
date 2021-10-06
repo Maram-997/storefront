e47dc289eee4cbc2ee99f651e5c1acbbf63e3ec6
Maram Abu Murad <maramabumurad97@gmail.com>
2021-10-06 23:40:03 +0300

    initial commit

--- src/app.js
+++ src/app.js
@@ -5,16 +5,31 @@ import Category from './components/categories'
 import Products from './components/product'
 import ActiveCategory from './components/activeCategory'
 import SimpleCart from './components/cart'
+import ProductDetails from './components/ProductDetails'
+import ShoppingCart from './components/ShoppingCart'
+import { Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 export default props => {
-    return (
-      <>
-        
-            <Header />
-            <SimpleCart/>
-            <ActiveCategory/>
-            <Category />
-            <Products />
-            <Footer />
-      </>
-    )
-  };
+  return (
+    <>
+      <Router>
+        <Header />
+        <Switch>
+        <Route exact path='/'>
+        </Route>
+        <Route path='/product/:id'>
+            <ProductDetails/>
+        </Route>
+        <Route path='/cart'>
+        <ShoppingCart/>
+        </Route>
+        <SimpleCart />
+        <ActiveCategory />
+        <Category />
+        <Products />
+        </Switch>
+        <Footer />
+      </Router>
+    </>
+  )
+};
